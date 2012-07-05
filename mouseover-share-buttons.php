f4149dbd5595c3cc5f8e71babdf92498a3ac2f3b
Florian Hahn <flo@fhahn.com>
2012-07-05 14:18:55 +0200

    added load ajax-widgets.js

--- mouseover-share-buttons.php
+++ mouseover-share-buttons.php
@@ -14,4 +14,13 @@ $plugin_dir = dirname(__FILE__);
 
 @require_once "$plugin_dir/options.php";
 
+function ngsb_sharebuttons_init(){
+  wp_register_script('ajax-share-buttons', plugins_url(basename(dirname(__FILE__))) . '/js/ajax-widgets.js', array(
+    'jquery',
+  ));
+  wp_enqueue_script('ajax-share-buttons');
+}
+
+add_action('init', 'ngsb_sharebuttons_init');
+
 ?>
